8fe57872d04c115c70b6ec1c24cedbe0ed0ce77c
gk646 <gk646@proton.me>
2022-12-31 04:16:46 +0100

    first commmit

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,14 +1,14 @@
-import tensorflow as tf
-import pygame
+import random
+
 import numpy as np
+from keras import initializers
 from tensorflow import keras
 
 
 def build_model():
-    # build the model
     model = keras.Sequential()
-    model.add(keras.layers.Flatten(input_shape=(50 ,50)))
-    model.add(keras.layers.Dense(units=100*100, activation='relu'))
+    model.add(keras.layers.Flatten(input_shape=(50, 50)))
+    model.add(keras.layers.Dense(units=50 * 50, activation='relu'))
     model.add(keras.layers.Dense(units=100, activation='relu'))
     model.add(keras.layers.Dense(units=100, activation='relu'))
     model.add(keras.layers.Dense(units=100, activation='relu'))
@@ -21,10 +21,73 @@ def build_model():
                   loss='binary_crossentropy',
                   metrics=['accuracy'])
     return model
+
+
 def test():
     model = build_model()
-
-    array = np.random.randint(0,100,(50,50))
+    array = np.random.randint(0, 100, (50, 50))
     model.predict(array)
 
 
+def cross_over(model1, model2):
+    weights1 = model1.get_weights()
+    weights2 = model2.get_weights()
+
+    crossover_point = random.randint(0, len(weights1))
+
+    temp_weights = weights1[crossover_point:]
+    weights1[crossover_point:] = weights2[crossover_point:]
+    weights2[crossover_point:] = temp_weights
+
+    model1.set_weights(weights1)
+    model2.set_weights(weights2)
+
+    model1.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model2.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return model1, model2
+
+
+def get_random_model():
+    new_model = keras.Sequential()
+    new_model.add(keras.layers.Flatten(input_shape=(50, 50)))
+    new_model.add(
+        keras.layers.Dense(units=50 * 50, activation='relu', kernel_initializer=initializers.random_normal))
+    new_model.add(
+        keras.layers.Dense(units=100, activation='relu', kernel_initializer=initializers.random_normal))
+    new_model.add(
+        keras.layers.Dense(units=100, activation='relu', kernel_initializer=initializers.random_normal))
+    new_model.add(
+        keras.layers.Dense(units=100, activation='relu', kernel_initializer=initializers.random_normal))
+    new_model.add(
+        keras.layers.Dense(units=100, activation='relu', kernel_initializer=initializers.random_normal))
+    new_model.add(
+        keras.layers.Dense(units=100, activation='relu', kernel_initializer=initializers.random_normal))
+    new_model.add(
+        keras.layers.Dense(units=100, activation='relu', kernel_initializer=initializers.random_normal))
+    new_model.add(keras.layers.Dense(units=4, activation='softmax'))
+
+    new_model.compile(optimizer='adam',
+                      loss='binary_crossentropy',
+                      metrics=['accuracy'])
+    return new_model
+
+
+def half_random_model(model1):
+    model2 = get_random_model()
+    weights1 = model1.get_weights()
+    weights2 = model2.get_weights()
+    crossover_point = random.randint(0, len(weights1))
+
+    # Crossover the weights
+    temp_weights = weights1[crossover_point:]
+    weights1[crossover_point:] = weights2[crossover_point:]
+    weights2[crossover_point:] = temp_weights
+
+    # Set the weights of the models
+    model1.set_weights(weights1)
+    model2.set_weights(weights2)
+
+    # Compile and train the models
+    model1.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model2.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return model1
--- SnakeGame.py
+++ SnakeGame.py
@@ -1,22 +1,29 @@
 import sys
-
+import os
+import keras
 import pygame
 import time
 import random
 import NeuralNetwork
 import numpy as np
 import cv2
+import tensorflow as tf
+
+tf.compat.v1.logging.set_verbosity(0)
+tf.compat.v1.logging.set_verbosity('ERROR')
 
 
 class SnakeGame:
-    def __init__(self, tickrate):
-
+    def __init__(self, tick_rate):
+        self.game_over_variable = 0
         self.snake_movement = 1
-        self.snake_speed = tickrate
+        self.snake_speed = tick_rate
+        self.highest_score1 = 0
+        self.highest_score2 = 0
         pygame.font.init()
         # Window size
-        self.window_x = 500
-        self.window_y = 500
+        self.window_x = 1000
+        self.window_y = 1000
 
         self.window = pygame.display.set_mode((self.window_x, self.window_y))
         # defining colors
@@ -30,242 +37,97 @@ class SnakeGame:
         self.surface = pygame.Surface((50, 50))
 
         self.scaled_surface = pygame.transform.scale(self.surface, (200, 200))
-        # FPS (frames per second) controller
         self.fps = pygame.time.Clock()
-
-        # defining snake default position
         self.snake_position = [25, 25]
-
-        # defining first 4 blocks of snake body
         self.snake_body = [[25, 25],
                            [24, 25],
                            [23, 25],
                            [22, 25]]
-        # fruit position
         self.fruit_position = [random.randrange(1, 49),
                                random.randrange(1, 49)]
-
         self.fruit_spawn = True
-
-        # setting default snake direction towards
-        # right
         self.direction = 'RIGHT'
         self.change_to = self.direction
-
-        # initial score
         self.score = 0
+        self.moved_fields = 0
 
-    def show_score(self, color, font, size):
-        # creating font object score_font
-        score_font = pygame.font.SysFont(font, size)
+    def rest_of_game(self):
+        if self.change_to == 'UP' and self.direction != 'DOWN':
+            self.direction = 'UP'
+        if self.change_to == 'DOWN' and self.direction != 'UP':
+            self.direction = 'DOWN'
+        if self.change_to == 'LEFT' and self.direction != 'RIGHT':
+            self.direction = 'LEFT'
+        if self.change_to == 'RIGHT' and self.direction != 'LEFT':
+            self.direction = 'RIGHT'
+        if self.moved_fields < 250:
+            self.moved_fields+=1
+        else:
+            self.score -=250
+            self.snake_position = [51,51]
+        # Moving the snake
+        if self.direction == 'UP':
+            self.snake_position[1] -= self.snake_movement
+        if self.direction == 'DOWN':
+            self.snake_position[1] += self.snake_movement
+        if self.direction == 'LEFT':
+            self.snake_position[0] -= self.snake_movement
+        if self.direction == 'RIGHT':
+            self.snake_position[0] += self.snake_movement
 
-        # create the display surface object
-        # score_surface
-        score_surface = score_font.render('Score : ' + str(self.score), True, color)
+        # Snake body growing mechanism
+        # if fruits and snakes collide then scores
+        # will be incremented by 10
+        self.snake_body.insert(0, list(self.snake_position))
+        if self.snake_position[0] == self.fruit_position[0] and self.snake_position[1] == self.fruit_position[1]:
+            self.score += 1
+            self.fruit_spawn = False
+        else:
+            self.snake_body.pop()
 
-        # create a rectangular object for the text
-        # surface object
-        score_rect = score_surface.get_rect()
+        if not self.fruit_spawn:
+            self.fruit_position = self.fruit_position = [random.randrange(1, 50),
+                                                         random.randrange(1, 50)]
 
-        # displaying text
-        self.surface.blit(score_surface, score_rect)
+        self.fruit_spawn = True
+        self.surface.fill(self.black)
 
-    def game_over(self):
-        # creating font object my_font
-        my_font = pygame.font.SysFont('times new roman', 50)
+        for pos in self.snake_body:
+            pygame.draw.rect(self.surface, self.green,
+                             pygame.Rect(pos[0], pos[1], 1, 1))
+            pygame.draw.rect(self.surface, self.white, pygame.Rect(
+                self.fruit_position[0], self.fruit_position[1], 1, 1))
 
-        # creating a text surface on which text
-        # will be drawn
-        game_over_surface = my_font.render(
-            'Your Score is : ' + str(self.score), True, self.red)
-
-        # create a rectangular object for the text
-        # surface object
-        game_over_rect = game_over_surface.get_rect()
-
-        # setting position of the text
-        game_over_rect.midtop = (self.window_x / 2, self.window_y / 4)
-
-        # blit will draw the text on screen
-        self.surface.blit(game_over_surface, game_over_rect)
-        pygame.display.flip()
-
-        # after 2 seconds we will quit the program
-        time.sleep(2)
-
-        # deactivating pygame library
-        pygame.quit()
-
-        # quit the program
-        quit()
-
-    def play(self):
-        # Main Function
-
-        while True:
-            print(self.get_game_state().shape)
-            # handling key events
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
-                        self.change_to = 'UP'
-                    if event.key == pygame.K_DOWN:
-                        self.change_to = 'DOWN'
-                    if event.key == pygame.K_LEFT:
-                        self.change_to = 'LEFT'
-                    if event.key == pygame.K_RIGHT:
-                        self.change_to = 'RIGHT'
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-
-            # If two keys pressed simultaneously
-            # we don't want snake to move into two
-            # directions simultaneously
-            if self.change_to == 'UP' and self.direction != 'DOWN':
-                self.direction = 'UP'
-            if self.change_to == 'DOWN' and self.direction != 'UP':
-                self.direction = 'DOWN'
-            if self.change_to == 'LEFT' and self.direction != 'RIGHT':
-                self.direction = 'LEFT'
-            if self.change_to == 'RIGHT' and self.direction != 'LEFT':
-                self.direction = 'RIGHT'
-
-            # Moving the snake
-            if self.direction == 'UP':
-                self.snake_position[1] -= self.snake_movement
-            if self.direction == 'DOWN':
-                self.snake_position[1] += self.snake_movement
-            if self.direction == 'LEFT':
-                self.snake_position[0] -= self.snake_movement
-            if self.direction == 'RIGHT':
-                self.snake_position[0] += self.snake_movement
-
-            # Snake body growing mechanism
-            # if fruits and snakes collide then scores
-            # will be incremented by 10
-            self.snake_body.insert(0, list(self.snake_position))
-            if self.snake_position[0] == self.fruit_position[0] and self.snake_position[1] == self.fruit_position[1]:
-                self.score += 1
-                self.fruit_spawn = False
-            else:
-                self.snake_body.pop()
-
-            if not self.fruit_spawn:
-                self.fruit_position = self.fruit_position = [random.randrange(1, 50),
-                                                             random.randrange(1, 50)]
-
-            self.fruit_spawn = True
-            self.surface.fill(self.black)
-
-            for pos in self.snake_body:
-                pygame.draw.rect(self.surface, self.green,
-                                 pygame.Rect(pos[0], pos[1], 1, 1))
-                pygame.draw.rect(self.surface, self.white, pygame.Rect(
-                    self.fruit_position[0], self.fruit_position[1], 1, 1))
-
-            # Game Over conditions
-            if self.snake_position[0] < 0 or self.snake_position[0] > 50:
-                self.game_over()
-            if self.snake_position[1] < 0 or self.snake_position[1] > 50:
-                self.game_over()
-
-            # Touching the snake body
-            for block in self.snake_body[1:]:
-                if self.snake_position[0] == block[0] and self.snake_position[1] == block[1]:
-                    self.game_over()
-
-            # displaying score countinuously
-            self.show_score(self.white, 'times new roman', 2)
-
-            scaled_win = pygame.transform.scale(self.surface, self.window.get_size())
-            self.window.blit(scaled_win, (0, 0))
-            pygame.display.flip()
-            # Refresh game screen
-            pygame.display.update()
-            # Frame Per Second /Refresh Rate
-            self.fps.tick(self.snake_speed)
+    def get_game_state(self):
+        screen_array = pygame.surfarray.array3d(self.surface)
+        gray_array = cv2.cvtColor(screen_array, cv2.COLOR_BGR2GRAY)
+        gray_array = gray_array.reshape(1, 50, 50)
+        return gray_array
 
     def play_with_ai(self, model):
-        # Main Function
-
-        while True:
-            # Get the output of the model
+        while not self.game_over_variable:
             output = model.predict(self.get_game_state())
-
-            # Determine the action to take
             action = np.argmax(output)
-
-            # Map the action to a key input
             if action == 0:
-                # Move up
                 self.change_to = 'UP'
             elif action == 1:
-                # Move down
                 self.change_to = 'DOWN'
             elif action == 2:
-                # Move left
                 self.change_to = 'LEFT'
             elif action == 3:
-                # Move right
                 self.change_to = 'RIGHT'
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-
-            if self.change_to == 'UP' and self.direction != 'DOWN':
-                self.direction = 'UP'
-            if self.change_to == 'DOWN' and self.direction != 'UP':
-                self.direction = 'DOWN'
-            if self.change_to == 'LEFT' and self.direction != 'RIGHT':
-                self.direction = 'LEFT'
-            if self.change_to == 'RIGHT' and self.direction != 'LEFT':
-                self.direction = 'RIGHT'
-
-                # Moving the snake
-            if self.direction == 'UP':
-                self.snake_position[1] -= self.snake_movement
-            if self.direction == 'DOWN':
-                self.snake_position[1] += self.snake_movement
-            if self.direction == 'LEFT':
-                self.snake_position[0] -= self.snake_movement
-            if self.direction == 'RIGHT':
-                self.snake_position[0] += self.snake_movement
-
-            self.snake_body.insert(0, list(self.snake_position))
-            if self.snake_position[0] == self.fruit_position[0] and self.snake_position[1] == self.fruit_position[1]:
-                self.score += 1
-                self.fruit_spawn = False
-            else:
-                self.snake_body.pop()
-
-            if not self.fruit_spawn:
-                self.fruit_position = self.fruit_position = [random.randrange(1, 50),
-                                                             random.randrange(1, 50)]
-
-            self.fruit_spawn = True
-            self.surface.fill(self.black)
-
-            for pos in self.snake_body:
-                pygame.draw.rect(self.surface, self.green,
-                                 pygame.Rect(pos[0], pos[1], 1, 1))
-                pygame.draw.rect(self.surface, self.white, pygame.Rect(
-                    self.fruit_position[0], self.fruit_position[1], 1, 1))
-
-            # Game Over conditions
+            self.rest_of_game()
             if self.snake_position[0] < 0 or self.snake_position[0] > 50:
-                self.game_over()
+                break
             if self.snake_position[1] < 0 or self.snake_position[1] > 50:
-                self.game_over()
-
-            # Touching the snake body
+                break
             for block in self.snake_body[1:]:
                 if self.snake_position[0] == block[0] and self.snake_position[1] == block[1]:
-                    self.game_over()
-
-            # displaying score countinuously
-            self.show_score(self.white, 'times new roman', 2)
+                    break
 
             scaled_win = pygame.transform.scale(self.surface, self.window.get_size())
             self.window.blit(scaled_win, (0, 0))
@@ -275,13 +137,125 @@ class SnakeGame:
             # Frame Per Second /Refresh Rate
             self.fps.tick(self.snake_speed)
 
-    def get_game_state(self):
-        screen_array = pygame.surfarray.array3d(self.surface)
-        gray_array = cv2.cvtColor(screen_array, cv2.COLOR_BGR2GRAY)
-        gray_array =  gray_array.reshape(1,50,50)
-        return gray_array
+    def reset_game(self):
+        self.snake_position = [25, 25]
+        self.snake_body = [[25, 25],
+                           [24, 25],
+                           [23, 25],
+                           [22, 25]]
+        self.fruit_position = [random.randrange(1, 49),
+                               random.randrange(1, 49)]
+        self.fruit_spawn = True
+        self.direction = 'RIGHT'
+        self.change_to = self.direction
+        self.score = 0
+        self.moved_fields = 0
+
+    def train_models(self, model):
+        crossover_1 = NeuralNetwork.get_random_model()
+        crossover_2 = NeuralNetwork.get_random_model()
+        for b in range(10):
+            scores = []
+            weights = []
+            crossover_1, crossover_2 = NeuralNetwork.cross_over(crossover_1, crossover_2)
+            for i in range(25):
+                crossover_1 = NeuralNetwork.half_random_model(crossover_1)
+                print("First Loop - " + str(i))
+                self.reset_game()
+                while True:
+                    output = crossover_1.predict(self.get_game_state(), verbose=0)
+                    action = np.argmax(output)
+                    if action == 0:
+                        self.change_to = 'UP'
+                    elif action == 1:
+                        self.change_to = 'DOWN'
+                    elif action == 2:
+                        self.change_to = 'LEFT'
+                    elif action == 3:
+                        self.change_to = 'RIGHT'
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            pygame.quit()
+                            sys.exit()
+                    self.rest_of_game()
+                    if self.snake_position[0] < 0 or self.snake_position[0] > 50:
+                        break
+                    if self.snake_position[1] < 0 or self.snake_position[1] > 50:
+                        break
+
+                    # Touching the snake body
+                    for block in self.snake_body[1:]:
+                        if self.snake_position[0] == block[0] and self.snake_position[1] == block[1]:
+                            break
+
+                    scaled_win = pygame.transform.scale(self.surface, self.window.get_size())
+                    self.window.blit(scaled_win, (0, 0))
+                    pygame.display.flip()
+                    # Refresh game screen
+                    pygame.display.update()
+                    # Frame Per Second /Refresh Rate
+                    self.fps.tick(self.snake_speed)
+                score = self.score * 1000 + self.moved_fields
+                weights.append(model.get_weights())
+                scores.append(score)
+            scores = np.array(scores)
+            print(scores.mean())
+            crossover_1 = NeuralNetwork.build_model()
+            crossover_1.set_weights(weights[scores.argmax()])
+            scores = []
+            weights = []
+            for i in range(25):
+                crossover_2 = NeuralNetwork.half_random_model(crossover_2)
+                print("Second Loop - " + str(i))
+                self.reset_game()
+                while True:
+                    output = crossover_2.predict(self.get_game_state(), verbose=0)
+                    action = np.argmax(output)
+                    if action == 0:
+                        self.change_to = 'UP'
+                    elif action == 1:
+                        self.change_to = 'DOWN'
+                    elif action == 2:
+                        self.change_to = 'LEFT'
+                    elif action == 3:
+                        self.change_to = 'RIGHT'
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            pygame.quit()
+                            sys.exit()
+                    self.rest_of_game()
+                    if self.snake_position[0] < 0 or self.snake_position[0] > 50:
+                        break
+                    if self.snake_position[1] < 0 or self.snake_position[1] > 50:
+                        break
+
+                    # Touching the snake body
+                    for block in self.snake_body[1:]:
+                        if self.snake_position[0] == block[0] and self.snake_position[1] == block[1]:
+                            break
+
+                    scaled_win = pygame.transform.scale(self.surface, self.window.get_size())
+                    self.window.blit(scaled_win, (0, 0))
+                    pygame.display.flip()
+                    # Refresh game screen
+                    pygame.display.update()
+                    # Frame Per Second /Refresh Rate
+                    self.fps.tick(self.snake_speed)
+                score = self.score * 1000 + self.moved_fields
+                weights.append(model.get_weights())
+                scores.append(score)
+            scores = np.array(scores)
+            print(scores.mean())
+            crossover_2 = NeuralNetwork.build_model()
+            crossover_2.set_weights(weights[scores.argmax()])
 
 
-game = SnakeGame(10)
-snakeNet = NeuralNetwork.build_model()
-game.play_with_ai(snakeNet)
+        best_model,best_model2 = NeuralNetwork.cross_over(crossover_1,crossover_2)
+        best_model.save('genetic1')
+
+
+game = SnakeGame(200)
+snakeNet = NeuralNetwork.get_random_model()
+game.train_models(model=snakeNet)
+
+# snakeNet.save("snakeModel")
